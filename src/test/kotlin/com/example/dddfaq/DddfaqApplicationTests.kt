f25098ddcbfe76c50327edbdae2a2a336782055b
gitkado <gitkado@gmail.com>
2021-11-10 23:30:46 +0900

    feat: first commit

---
+++ src/test/kotlin/com/example/dddfaq/DddfaqApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.dddfaq
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DddfaqApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
